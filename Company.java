100b6dd634e89934113672c72b2c04391e9efede
Dziaba <wyrwisz.hubert@gmail.com>
2024-05-04 13:08:10 +0000

    zad3

--- Company.java
+++ Company.java
@@ -8,12 +8,29 @@ public class Company{
 
         employees[0] = new Manager("Kowalski", 76000, 0);
         employees[1] = new Worker("Jan Nowak", 5700 , "Worker");
-        employees[2] = new Employee("Marek M", 2100);
+        employees[2] = new Worker("Marek M", 2100, "Worker");
         employees[3] = new Employee("Hubert W", 9000);
         employees[4] = new Worker(" Bob", 7600, "Worker");
 
 
-        System.err.println("Wszyscy pracownicy:");
+        int nonMenager = 0;
+        
+        for (int i=0; i<employees.length; i++){
+            if(!(employees[i] instanceof Manager)){
+                nonMenager ++;
+            }
+        }
+        System.out.println("Number of non menagers:" + nonMenager);
+
+        if (employees[0] instanceof Manager){
+            Manager manager = (Manager) employees[0];
+            manager.setNumberOfSubordinates(nonMenager);
+            manager.setSalary(5000);
+
+        }
+
+
+        System.out.println("Wszyscy pracownicy:");
 
         for (int i=0; i<employees.length; i++){
             System.out.println(employees[i]);
